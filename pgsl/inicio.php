5f621b26d30aa5a816592746fa92aa9c36bf4149
Joary <netinho_brak2@hotmail.com>
2020-10-25 15:01:53 -0400

    Update inicio.php

--- pgsl/inicio.php
+++ pgsl/inicio.php
@@ -33,7 +33,7 @@ if (basename($_SERVER["REQUEST_URI"]) === basename(__FILE__))
 <div class="container pt-5">
             <div class="row">
                 <?
-				$SQL = "SELECT cliente.nome,agendamento.inicio FROM agendamento inner join clientes on clientes.codigo=agendamento.cliente";
+				$SQL = "SELECT clientes.nome,agendamento.inicio FROM agendamento inner join clientes on clientes.codigo=agendamento.cliente";
 				$RES = mysqli_query($db3,$SQL);
 				while($row = mysqli_fetch_array($RES))
 				{
