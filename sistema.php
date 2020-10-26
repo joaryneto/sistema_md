e996bd7570705a0f7d7f9f7598f9bb5322cb2328
hismet <hismetcba@users.noreply.github.com>
2020-10-26 12:48:07 -0400

    Update sistema.php

--- sistema.php
+++ sistema.php
@@ -48,10 +48,16 @@ function revertedata($data){
     <!-- Roboto fonts CSS -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&amp;display=swap" rel="stylesheet">
 
-    <link href="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.css" rel="stylesheet"/>
     <!-- Bootstrap core CSS -->
     <link href="template/vendor/bootstrap-4.4.1/css/bootstrap.min.css" rel="stylesheet">
 
+    <link href="template/vendor/select2/dist/css/select2.min.css" rel="stylesheet" type="text/css" />
+    <link href="template/vendor/switchery/dist/switchery.min.css" rel="stylesheet" />
+    <link href="template/vendor/bootstrap-select/bootstrap-select.min.css" rel="stylesheet" />
+    <link href="template/vendor/bootstrap-tagsinput/dist/bootstrap-tagsinput.css" rel="stylesheet" />
+    <link href="template/vendor/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.css" rel="stylesheet" />
+    <link href="template/vendor/multiselect/css/multi-select.css" rel="stylesheet" type="text/css" />
+	
     <!-- Swiper CSS -->
     <link href="template/vendor/swiper/css/swiper.min.css" rel="stylesheet">
 
@@ -333,15 +339,6 @@ function revertedata($data){
 									<div class="row">
 									<div class="col-12">
 									  <div class="form-group col-md-4 m-t-20">
-    <label>Languages</label>
-    <input class="form-control autocomplete" placeholder="Enter A" />
-  </div>
-  
-  <div class="form-group">
-    <label >Another Field</label>
-    <input class="form-control">
-  </div>
-</div>
 									<div class="form-group col-md-4 m-t-20">
 									<div class="container">
 									<select name="situacao" class="form-control" autocomplete="off"  style="width: 100%; height:36px;" required="required">
@@ -359,7 +356,7 @@ function revertedata($data){
 									    <input name="user" type="text" autocomplete="off" class="form-control" onkeyup="javascript: ajaxLoader('?br=atu_pesquisa&pesquisa='+ this.value +'&ap=1','situacao','GET');" required="required" />
 									</div>
 									<div class="form-group col-md-12 m-t-20"><label>Cliente: </label>
-									<select name="situacao" id="situacao" autocomplete="off" class="form-control" style="width: 100%; height:36px;" required="required" />
+									<select name="situacao" id="situacao" autocomplete="off" class="select2 form-control custom-select" style="width: 100%; height:36px;" required="required" />
 										  <option>Selecione o Cliente</option>
 										</select>
 									</div>
@@ -452,7 +449,119 @@ function revertedata($data){
     <!-- swiper js -->
     <script src="template/vendor/sparklines/jquery.sparkline.min.js"></script>
 	
-
+    <script src="template/vendor/select2/dist/js/select2.full.min.js" type="text/javascript"></script>
+    <script src="template/vendor/bootstrap-select/bootstrap-select.min.js" type="text/javascript"></script>
+    <script src="template/vendor/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js"></script>
+    <script src="template/vendor/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.js" type="text/javascript"></script>
+    <script type="text/javascript" src="template/vendor/multiselect/js/jquery.multi-select.js"></script>
+    <script>
+    $(function() {
+        // Switchery
+        var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+        $('.js-switch').each(function() {
+            new Switchery($(this)[0], $(this).data());
+        });
+        // For select 2
+        $(".select2").select2();
+        $('.selectpicker').selectpicker();
+        //Bootstrap-TouchSpin
+        $(".vertical-spin").TouchSpin({
+            verticalbuttons: true,
+            verticalupclass: 'ti-plus',
+            verticaldownclass: 'ti-minus'
+        });
+        var vspinTrue = $(".vertical-spin").TouchSpin({
+            verticalbuttons: true
+        });
+        if (vspinTrue) {
+            $('.vertical-spin').prev('.bootstrap-touchspin-prefix').remove();
+        }
+        $("input[name='tch1']").TouchSpin({
+            min: 0,
+            max: 100,
+            step: 0.1,
+            decimals: 2,
+            boostat: 5,
+            maxboostedstep: 10,
+            postfix: '%'
+        });
+        $("input[name='tch2']").TouchSpin({
+            min: -1000000000,
+            max: 1000000000,
+            stepinterval: 50,
+            maxboostedstep: 10000000,
+            prefix: '$'
+        });
+        $("input[name='tch3']").TouchSpin();
+        $("input[name='tch3_22']").TouchSpin({
+            initval: 40
+        });
+        $("input[name='tch5']").TouchSpin({
+            prefix: "pre",
+            postfix: "post"
+        });
+        // For multiselect
+        $('#pre-selected-options').multiSelect();
+        $('#optgroup').multiSelect({
+            selectableOptgroup: true
+        });
+        $('#public-methods').multiSelect();
+        $('#select-all').on('click', function() {
+            $('#public-methods').multiSelect('select_all');
+            return false;
+        });
+        $('#deselect-all').on('click', function() {
+            $('#public-methods').multiSelect('deselect_all');
+            return false;
+        });
+        $('#refresh').on('click', function() {
+            $('#public-methods').multiSelect('refresh');
+            return false;
+        });
+        $('#add-option').on('click', function() {
+            $('#public-methods').multiSelect('addOption', {
+                value: 42,
+                text: 'test 42',
+                index: 0
+            });
+            return false;
+        });
+        $(".ajax").select2({
+            ajax: {
+                url: "https://api.github.com/search/repositories",
+                dataType: 'json',
+                delay: 250,
+                data: function(params) {
+                    return {
+                        q: params.term, // search term
+                        page: params.page
+                    };
+                },
+                processResults: function(data, params) {
+                    // parse the results into the format expected by Select2
+                    // since we are using custom formatting functions we do not need to
+                    // alter the remote JSON data, except to indicate that infinite
+                    // scrolling can be used
+                    params.page = params.page || 1;
+                    return {
+                        results: data.items,
+                        pagination: {
+                            more: (params.page * 30) < data.total_count
+                        }
+                    };
+                },
+                cache: true
+            },
+            escapeMarkup: function(markup) {
+                return markup;
+            }, // let our custom formatter work
+            minimumInputLength: 1,
+            templateResult: formatRepo, // omitted for brevity, see the source of this page
+            templateSelection: formatRepoSelection // omitted for brevity, see the source of this page
+        });
+    });
+    </script>
+	
     <!-- template custom js -->
     <script src="template/js/main.js"></script>
 
@@ -661,23 +770,7 @@ function onCalendarDayClick(date, allDay, jsEvent, view) {
   });
    
   </script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/js/bootstrap.js"></script>
-<script>
-$(function() {
-  var availableTags = [
-    "ActionScript", "AppleScript", "Asp", "BASIC", "C", "C++",
-    "Clojure", "COBOL", "ColdFusion", "Erlang", "Fortran",
-    "Groovy", "Haskell", "Java", "JavaScript", "Lisp", "Perl",
-    "PHP", "Python", "Ruby", "Scala", "Scheme"
-  ];
-  
-  $(".autocomplete").autocomplete({
-    source: availableTags
-  });
-});
-</script>
+
 
 </body>
 
