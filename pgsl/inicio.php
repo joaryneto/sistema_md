e33f2ec4ea9b8f9da186c16a5250ff908c1d0279
Joary <hismetcba@users.noreply.github.com>
2020-10-30 12:36:24 -0400

    Update inicio.php

--- pgsl/inicio.php
+++ pgsl/inicio.php
@@ -54,7 +54,7 @@ if (basename($_SERVER["REQUEST_URI"]) === basename(__FILE__))
                         <div class="col pl-0">
                             <b class="h4 mb-3 font-weight-normal"><? echo $row['nome'];?></b>
                             <p class="large text-mute" style="font-size: initial;">Dia: <? echo formatodatahora($row['data']);?> às Hora: <? echo formatohora($row['hora']);?>hs</p>
-							<button type="button" onclick="whats('<? echo str_replace("(","", str_replace(")","", str_replace("-","",$row['celular'])));?>','Bom dia <? echo $row['nome'];?>! Passando para lembrar que você tem horário agendado hoje as <? echo formatohora($row['hora']);?>hs. Studio KA');" class="mb-2 btn btn-outline-success  rounded-0">Whats <i class="fa fa-whatsapp" aria-hidden="true"></i></button>
+							<button type="button" onclick="whats('<? echo str_replace("(","", str_replace(")","", str_replace("-","",$row['celular'])));?>','Bom dia *<? echo $row['nome'];?>*! Passando para lembrar que você tem horário agendado hoje as *<? echo formatohora($row['hora']);?>hs*. Studio KA');" class="mb-2 btn btn-outline-success  rounded-0">Whats <i class="fa fa-whatsapp" aria-hidden="true"></i></button>
                             <button type="button" onclick="agenda(2,'<? echo $row['codigo'];?>','<? echo $row['cliente'];?>','<? echo $row['data'];?>','<? echo $row['hora'];?>','<? echo $row['nome'];?>');" class="mb-2 btn btn-outline-primary rounded-0">Editar</button>
 							<button type="button" id="" class="mb-2 btn btn-outline-danger rounded-0">Excluir</button>
                         </div>
