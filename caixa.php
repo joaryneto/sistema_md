9eaa396723b33ff9c6f7bd882c6f057c499a9b39
Joary <netinho_brak2@hotmail.com>
2020-11-04 19:48:51 -0400

    Update caixa.php

--- caixa.php
+++ caixa.php
@@ -510,6 +510,43 @@ function auto()
 								   </div>
 								 </div>
 								</div>
+								<p id="demo">Clique no botão para receber as coordenadas:</p>
+<button onclick="getLocation()">Clique Aqui</button>
+<script>
+var x=document.getElementById("demo");
+function getLocation()
+  {
+  if (navigator.geolocation)
+    {
+    navigator.geolocation.getCurrentPosition(showPosition,showError);
+    }
+  else{x.innerHTML="Seu browser não suporta Geolocalização.";}
+  }
+function showPosition(position)
+  {
+  x.innerHTML="Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;  
+  }
+function showError(error)
+  {
+  switch(error.code) 
+    {
+    case error.PERMISSION_DENIED:
+      x.innerHTML="Usuário rejeitou a solicitação de Geolocalização."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML="Localização indisponível."
+      break;
+    case error.TIMEOUT:
+      x.innerHTML="A requisição expirou."
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML="Algum erro desconhecido aconteceu."
+      break;
+    }
+  }
+</script>
+
 								</div>
 								<div class="m-t-40 row">
 								<div class="input-group col-md-12 m-t-20">
