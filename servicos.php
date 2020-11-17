70b9b849cb64cd7fe9278170c0648846ed0a61eb
Joary <hismetcba@users.noreply.github.com>
2020-11-17 14:44:41 -0400

    Update servicos.php

--- servicos.php
+++ servicos.php
@@ -13,16 +13,19 @@
 
     <!-- sidebar -->
 <div class="container pt-5">
-
 <div class="row">
-<div class="col-md-12 col-sm-12"> 
-<div class="component-box">
-<a class="btn pmd-btn-outline pmd-ripple-effect btn-primary" href="index.php?sistema=agsge"><i class="fa fa-plus-circle"></i> Sistema Escolar</a>
-<a class="btn pmd-btn-outline pmd-ripple-effect btn-primary" href="index.php?sistema=agsl"><i class="fa fa-plus-circle"></i> Sistema de Salão</a>					
-</div>	
+	<div class="col-md-12">
+		<!--List group items with heading and Linking start -->
+		<div class="list-group pmd-z-depth pmd-list pmd-card-list"> <a href="index.php?sistema=agsge" class="list-group-item ">
+			<h3 class="list-group-item-heading">Sistema de Gerenciamento de Escola</h3>
+			<p class="list-group-item-text"></p>
+			</a> <a href="index.php?sistema=agsl" class="list-group-item">
+			<h3 class="list-group-item-heading">Sistema de Gerenciamento de Salão</h3>
+			<p class="list-group-item-text"></p>
+			</a>
+			</div>
+	</div>
 </div>
-</div>					    
-
 </body>
 
 </html>
