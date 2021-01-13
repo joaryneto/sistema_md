19a023ebf72e08df3ff8ff48de10bf11afc1d29f
Joary <hismetcba@users.noreply.github.com>
2021-01-13 17:05:14 -0400

    Update atu_fatura.php

--- pgsge/atu_fatura.php
+++ pgsge/atu_fatura.php
@@ -87,58 +87,62 @@ $options = [
   'sandbox' => true // altere conforme o ambiente (true = desenvolvimento e false = producao)
 ];
  
-$item_1 = [
-    'name' => 'Item 1', // nome do item, produto ou serviço
-    'amount' => 1, // quantidade
-    'value' => 1000 // valor (1000 = R$ 10,00) (Obs: É possível a criação de itens com valores negativos. Porém, o valor total da fatura deve ser superior ao valor mínimo para geração de transações.)
-];
- 
-$items =  [
-    $item_1,
-];
- 
-$customer = [
-  'name' => 'Gorbadoc Oldbuck', // nome do cliente
-  'cpf' => '94271564656' , // cpf do cliente
-  'phone_number' => '5144916523' // telefone do cliente
-];
-
-// Exemplo para receber notificações da alteração do status do carne.
-// $metadata = ['notification_url'=>'sua_url_de_notificacao_.com.br']
-// Outros detalhes em: https://dev.gerencianet.com.br/docs/notificacoes
-
-// Como enviar seu $body com o $metadata
-// $body = [
-// 'items' => $items,
-// 'customer' => $customer,
-// 'expire_at' => '2020-12-02',
-// 'repeats' => 5,
-// 'split_items' => false,
-// 'metadata' => $metadata
-// ];
-//echo $data = $faturavenc.'-28';
-
-$body = [
-  'items' => $items,
-  'customer' => $customer,
-  'expire_at' => $faturavenc.'-28', // data de vencimento da primeira parcela do carnê
-  'repeats' => $qtd, // número de parcelas do carnê
-  'split_items' => false
-];
-
-try {
-    $api = new Gerencianet($options);
-    $carnet = $api->createCarnet([], $body);
- 
-    print_r($carnet);
-	
-} catch (GerencianetException $e) {
-    print_r($e->code);
-    print_r($e->error);
-    print_r($e->errorDescription);
-} catch (Exception $e) {
-    print_r($e->getMessage());
-}
+   $item_1 = [
+       'name' => 'Item 1', // nome do item, produto ou serviço
+       'amount' => 1, // quantidade
+       'value' => 1000 // valor (1000 = R$ 10,00) (Obs: É possível a criação de itens com valores negativos. Porém, o valor total da fatura deve ser superior ao valor mínimo para geração de transações.)
+   ];
+   $items = [
+       $item_1
+   ];
+   $metadata = array('notification_url'=>'sua_url_de_notificacao_.com.br'); //Url de notificações
+   $customer = [
+       'name' => 'Gorbadoc Oldbuck', // nome do cliente
+       'cpf' => '94271564656', // cpf válido do cliente
+       'phone_number' => '5144916523', // telefone do cliente
+   ];
+   $discount = [ // configuração de descontos
+       'type' => 'currency', // tipo de desconto a ser aplicado
+       'value' => 599 // valor de desconto 
+   ];
+   $configurations = [ // configurações de juros e mora
+       'fine' => 200, // porcentagem de multa
+       'interest' => 33 // porcentagem de juros
+   ];
+   $conditional_discount = [ // configurações de desconto condicional
+       'type' => 'percentage', // seleção do tipo de desconto 
+       'value' => 500, // porcentagem de desconto
+       'until_date' => '2019-08-30' // data máxima para aplicação do desconto
+   ];
+   $bankingBillet = [
+       'expire_at' => $faturavenc, // data de vencimento do titulo
+       'message' => 'teste\nteste\nteste\nteste', // mensagem a ser exibida no boleto
+       'customer' => $customer,
+       'discount' =>$discount,
+       'conditional_discount' => $conditional_discount
+   ];
+   $payment = [
+       'banking_billet' => $bankingBillet // forma de pagamento (banking_billet = boleto)
+   ];
+   $body = [
+       'items' => $items,
+       'metadata' =>$metadata,
+       'payment' => $payment
+   ];
+   try {
+     $api = new Gerencianet($options);
+     $pay_charge = $api->oneStep([],$body);
+     echo '<pre>';
+     print_r($pay_charge);
+     echo '<pre>';
+     
+    } catch (GerencianetException $e) {
+       print_r($e->code);
+       print_r($e->error);
+       print_r($e->errorDescription);
+   } catch (Exception $e) {
+       print_r($e->getMessage());
+   }
 
  }
 }
