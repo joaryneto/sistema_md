79e071160dbf884520e59da24a7676a6926824d4
Joary <hismetcba@users.noreply.github.com>
2021-01-14 08:19:40 -0400

    Update atu_fatura.php

--- pgsge/atu_fatura.php
+++ pgsge/atu_fatura.php
@@ -87,62 +87,44 @@ $tipo = $_GET['tipo'];
         'sandbox' => true // altere conforme o ambiente (true = desenvolvimento e false = producao)
     ];
     
-   $item_1 = [
-       'name' => 'Item 1', // nome do item, produto ou serviço
-       'amount' => 1, // quantidade
-       'value' => 1000 // valor (1000 = R$ 10,00) (Obs: É possível a criação de itens com valores negativos. Porém, o valor total da fatura deve ser superior ao valor mínimo para geração de transações.)
-   ];
-   $items = [
-       $item_1
-   ];
-   $metadata = array('notification_url'=>'sua_url_de_notificacao_.com.br'); //Url de notificações
-   $customer = [
-       'name' => 'Gorbadoc Oldbuck', // nome do cliente
-       'cpf' => '94271564656', // cpf válido do cliente
-       'phone_number' => '5144916523', // telefone do cliente
-   ];
-   $discount = [ // configuração de descontos
-       'type' => 'currency', // tipo de desconto a ser aplicado
-       'value' => 599 // valor de desconto 
-   ];
-   $configurations = [ // configurações de juros e mora
-       'fine' => 200, // porcentagem de multa
-       'interest' => 33 // porcentagem de juros
-   ];
-   $conditional_discount = [ // configurações de desconto condicional
-       'type' => 'percentage', // seleção do tipo de desconto 
-       'value' => 500, // porcentagem de desconto
-       'until_date' => '2021-08-30' // data máxima para aplicação do desconto
-   ];
-   $bankingBillet = [
-       'expire_at' => '2021-07-01', // data de vencimento do titulo
-       'message' => 'teste\nteste\nteste\nteste', // mensagem a ser exibida no boleto
-       'customer' => $customer,
-       'discount' =>$discount,
-       'conditional_discount' => $conditional_discount
-   ];
-   $payment = [
-       'banking_billet' => $bankingBillet // forma de pagamento (banking_billet = boleto)
-   ];
-   $body = [
-       'items' => $items,
-       'metadata' =>$metadata,
-       'payment' => $payment
-   ];
-   try {
-     $api = new Gerencianet($options);
-     $pay_charge = $api->oneStep([],$body);
-     echo '<pre>';
-     print_r($pay_charge);
-     echo '<pre>';
-     
-    } catch (GerencianetException $e) {
-       print_r($e->code);
-       print_r($e->error);
-       print_r($e->errorDescription);
-   } catch (Exception $e) {
-       print_r($e->getMessage());
-   }
+$charge_id = '1226863';
+
+// $charge_id refere-se ao ID da transação gerada anteriormente
+$params = [
+  'id' => $charge_id
+];
+ 
+$customer = [
+  'name' => 'Joary', // nome do cliente
+  'cpf' => '05497813151' , // cpf válido do cliente
+  'phone_number' => '65999999104' // telefone do cliente
+];
+ 
+$bankingBillet = [
+  'expire_at' => '2021-2-12', // data de vencimento do boleto (formato: YYYY-MM-DD)
+  'customer' => $customer
+];
+ 
+$payment = [
+  'banking_billet' => $bankingBillet // forma de pagamento (banking_billet = boleto)
+];
+ 
+$body = [
+  'payment' => $payment
+];
+ 
+try {
+    $api = new Gerencianet($options);
+    $charge = $api->payCharge($params, $body);
+ 
+    print_r($charge);
+} catch (GerencianetException $e) {
+    print_r($e->code);
+    print_r($e->error);
+    print_r($e->errorDescription);
+} catch (Exception $e) {
+    print_r($e->getMessage());
+}
 
  //}
 }
