74beb7673443e9a0ef3049708a5588c5e9cd8e61
Joary <hismetcba@users.noreply.github.com>
2021-03-03 09:18:47 -0400

    Update index.php

--- template/layout/ecweb/index.php
+++ template/layout/ecweb/index.php
@@ -553,6 +553,16 @@
     <!-- Custom scripts for this template -->
     <script src="template/layout/ecweb/js/freelancer.min.js"></script>
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-72359706-4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-72359706-4');
+</script>
+
   </body>
 
 </html>
